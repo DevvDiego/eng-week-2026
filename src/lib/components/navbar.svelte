774635ae07ae4e4d27b2f5d5b9021c085c79c8ba
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-21 18:16:04 -0600

    add custom navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { resolve } from '$app/paths';
+
+</script>
+
+<header class="flex justify-between items-start  font-sans uppercase tracking-tighter">
+	<h2 class="text-lg font-bold italic tracking-normal">Drone-S</h2>
+	
+	<nav class="flex gap-6 sm:gap-10 text-xs tracking-widest">
+		<a href={resolve("/")} class="hover:opacity-50">MAIN</a>
+		<a href="#about" class="hover:opacity-50">ABOUT</a>
+		<!-- <a href="" class="hover:opacity-50">STORE</a>
+		<a href="" class="hover:opacity-50">CONTACT</a> -->
+	</nav>
+
+</header>
