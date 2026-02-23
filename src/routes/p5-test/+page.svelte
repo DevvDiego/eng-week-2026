265f817e6d6f1ebf729fc82cc4a62de065f78f7f
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-22 19:44:08 -0600

    Add base route that showcases the sketch component

---
+++ src/routes/p5-test/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Sketch from '$lib/components/sketch.svelte';
+  let bg = "#262626";
+  let color = '#00d8ff';
+</script>
+
+<h1>3D model viewer test</h1>
+
+<div class="controls">
+  <h1>background</h1>
+  <input type="color" bind:value={bg} />
+  
+  <h1>fill color</h1>
+  <input type="color" bind:value={color} />
+</div>
+
+<Sketch {bg} {color} />
