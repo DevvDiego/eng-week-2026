463cd57c02e84a62b07befeab22d920a7c130756
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-03-30 17:31:40 -0600

    Update routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -240,7 +240,7 @@
                 
                 <a href={"/fichaTecnica.pdf"} class="relative inline-block group/btn overflow-hidden border border-black/20 px-5 py-2.5 text-[10px] md:text-xs font-black tracking-[0.3em] transition-all duration-300 bg-transparent text-black hover:text-white">
                     <div class="absolute inset-0 bg-black translate-y-full group-hover/btn:translate-y-0 transition-transform duration-300 ease-out"></div>
-                    <span class="relative z-10">VER</span>
+                    <span class="relative z-10">VER MÁS</span>
                 </a>
             </div>
 
@@ -256,9 +256,9 @@
                     Una experiencia lúdica que complementa los conceptos del proyecto.
                 </p>
                 
-                <a href={"#"} class="relative inline-block group/btn overflow-hidden border border-black/20 px-5 py-2.5 text-[10px] md:text-xs font-black tracking-[0.3em] transition-all duration-300 bg-transparent text-black hover:text-white">
+                <a href={resolve("/game")} class="relative inline-block group/btn overflow-hidden border border-black/20 px-5 py-2.5 text-[10px] md:text-xs font-black tracking-[0.3em] transition-all duration-300 bg-transparent text-black hover:text-white">
                     <div class="absolute inset-0 bg-black translate-y-full group-hover/btn:translate-y-0 transition-transform duration-300 ease-out"></div>
-                    <span class="relative z-10">JUGAR AHORA</span>
+                    <span class="relative z-10">VER MÁS</span>
                 </a>
                 
             </div>
