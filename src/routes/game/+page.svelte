00f3c734c1b353aab33951071fb5c5db97d707d1
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-03-30 17:16:58 -0600

    Agg game route and image

---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+    import { resolve } from "$app/paths";
+
+</script>
+
+
+<section class="relative min-h-screen w-full bg-zinc-900 text-white font-sans uppercase overflow-hidden p-6 md:p-10">
+  
+    <div class="absolute inset-0 z-0">
+        <img src="game.jpg" alt="Laboratorio" class="h-full w-full object-cover opacity-60" />
+    </div>
+
+    <div class="relative z-10 grid grid-cols-1 md:grid-cols-2 h-full min-h-[80vh]">
+        
+        <div class="flex flex-col justify-center p-4 md:p-12">
+            <div class="max-w-md">
+                <h2 class="text-2xl md:text-3xl font-black tracking-tighter mb-4">
+                    El recorrido
+                </h2>
+                <p class="text-xs md:text-sm leading-relaxed tracking-widest opacity-90 font-medium">
+                    Este proyecto nace de la necesidad de integrar los conocimientos adquiridos 
+                    durante la carrera: electrónica analógica y digital, control, procesamiento 
+                    de señales, y también la comunicación visual de resultados a través de la 
+                    fotografía y el diseño.
+                </p>
+            </div>
+        </div>
+
+        <div class="relative flex flex-col justify-around items-center sm:items-end p-4 md:p-12 space-y-12 md:space-y-0">
+
+            <div class="relative z-10 text-center sm:text-right group">
+                <p class="text-[10px] md:text-xs font-bold tracking-[0.2em] mb-1 opacity-70">Fase 1</p>
+                <h3 class="text-sm md:text-lg font-black tracking-tighter">Concepto y Diseño</h3>
+            </div>
+
+            <div class="relative z-10 text-right max-w-50">
+                <h3 class="text-sm md:text-lg font-black tracking-tighter leading-none">
+                Desarrollo <br/> en Laboratorio
+                </h3>
+            </div>
+
+            <div class="relative z-10 text-right">
+                <h3 class="text-sm md:text-lg font-black tracking-tighter">
+                Documentación <br/> y Fotografía
+                </h3>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="relative z-10 mt-10 p-6 md:p-10 md:absolute md:bottom-12 md:left-12">
+        <div class="flex items-center justify-center sm:justify-start gap-4">
+            <p class="text-[10px] md:text-xs font-bold tracking-widest hover:text-zinc-400 transition-colors">
+                MEMORIA TÉCNICA Y <br/> GALERÍA DE IMÁGENES
+            </p>
+        </div>
+    </div>
+
+</section>
